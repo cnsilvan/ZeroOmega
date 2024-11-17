57e163b625899937f3c5ad011412e9e35b71e03b
cnsilvan <cnsilvan@gmail.com>
2024-11-17 23:48:54 +0800

    zip

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd omega-build
+npm run deps
+npm run dev
+grunt
+cd ../omega-target-chromium-extension
+cd build
+zip -r ../build.zip ./*
+mv build.zip ~
+cd ..
+minify-all-js ./build -j
+cd build && zip -r ../release.zip ./*
+mv release.zip ~
